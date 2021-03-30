1070cdfac5135e6b3095e217f21aa844d1e4727d
krishpranav <krisna.pranav@gmail.com>
2021-03-30 10:28:35 +0530

    done changes

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/hackingtool/
+
+clear
+
+cd /etc
+
+clear
+
+sudo rm -rf /etc/hackingtool
+
+clear
+
+mkdir hackingtool
+
+clear
+
+cd hackingtool
+
+clear
+
+git clone https://github.com/code-developers/hackingtool
+
+clear
+
+cd hackingtool
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
