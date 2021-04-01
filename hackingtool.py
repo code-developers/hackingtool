7ac9f99d8517f0cfd9cb8390ef2beb50d5523d2e
krishpranav <krisna.pranav@gmail.com>
2021-04-01 13:40:26 +0530

    adding imports

--- hackingtool.py
+++ hackingtool.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env/python
+# -*- coding: utf-8
 
+import os
+import webbrowser
+from platfrom import system
+from time import sleep
+
+from core import HackingToolsCollection
+from tools.anonsurf import AnonSurfTools
+from tools.ddos import DDOSTools
+from tools.exploit_frameworks import ExploitFrameworkTools
+from tools.forensic_tools import ForensicTools
+from tools.information_gathering_tools import InformationGatheringTools
+from tools.other_tools import OtherTools
+from tools.payload_creator import PayloadCreatorTools
+from tools.phising_attack import PhishingAttackTools
+from tools.post_exploitation import PostExploitationTools
+from tools.remote_administration import RemoteAdministrationTools
+from tools.reverse_engineering import ReverseEngineeringTools
+from tools.sql_tools import SqlInjectionTools
+from tools.steganography import SteganographyTools
+from tools.tool_manager import ToolManager
+from tools.webattack import WebAttackTools
+from tools.wireless_attack_tools import WirelessAttackTools
+from tools.wordlist_generator import WordlistGeneratorTools
+from tools.xss_attack import XSSAttackTools
